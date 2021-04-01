f32cc3056f25099fa325632a5ca7526c175c3b9a
zhangfangrui <zhangfangrui@gmail.com>
2021-04-01 09:07:23 +0800

    kotlin demo

--- src/main/kotlin/SpecialSyntax.kt
+++ src/main/kotlin/SpecialSyntax.kt
@@ -7,6 +7,24 @@
 
 fun main() {
     infixFunTest()
+    varArgPrintAll("Hello", "Hallo", "Salut", "Hola", "你好")
+    printAllWithPrefix(
+        "Hello", "Hallo", "Salut", "Hola", "你好",
+        prefix = "Greeting: "
+    )
+    nullSafetyTest()
+}
+
+fun log(vararg entries: String) {
+    varArgPrintAll(*entries)                                             // 5
+}
+
+fun printAllWithPrefix(vararg messages: String, prefix: String) {  // 3
+    for (m in messages) println(prefix + m)
+}
+
+fun varArgPrintAll(vararg messages: String) {                            // 1
+    for (m in messages) println(m)
 }
 
 fun infixFunTest(){
@@ -23,6 +41,20 @@ fun infixFunTest(){
     val sophia = Person("Sophia")
     val claudia = Person("Claudia")
     sophia likes claudia                                       // 5
+}
+
+fun nullSafetyTest(){
+    var neverNull: String = "This can't be null"            // 1
+    // neverNull = null                                        // 2
+    var nullable: String? = "You can keep a null here"      // 3
+    nullable = null                                         // 4
+    var inferredNonNull = "The compiler assumes non-null"   // 5
+    // inferredNonNull = null                                  // 6
+    fun strLength(notNull: String): Int {                   // 7
+        return notNull.length
+    }
+    strLength(neverNull)                                    // 8
+    //strLength(nullable)                                     // 9
 }
 
 class Person(val name: String) {
